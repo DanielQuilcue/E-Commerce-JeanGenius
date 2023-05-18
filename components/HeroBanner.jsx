af6e564058873c04b2671c73abffc16d5971a463
DanielQuilcue <danielquilcue18@gmail.com>
2023-05-18 18:43:42 -0500

    Category && Products

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -11,37 +11,11 @@ export default function HeroBanner() {
         showThumbs={false}
         showIndicators={false}
         showStatus={false}
-        renderArrowPrev={(clickHandler, hasPrev) => (
-          <div
-            onClick={clickHandler}
-            className="absolute right-[31px] md:right-[51px] bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
-          >
-            <BiArrowBack className="text-sm md:text-lg" />
-          </div>
-        )}
-        renderArrowNext={(clickHandler, hasNext) => (
-          <div
-            onClick={clickHandler}
-            className="absolute right-0 bottom-0 w-[30px] md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center justify-center cursor-pointer hover:opacity-90"
-          >
-            <BiArrowBack className="rotate-180 text-sm md:text-lg" />
-          </div>
-        )}
       >
         <div>
           <img
-            src="/slider/jeans1.jpg"
-            className=" h-[500px] aspect-[16/10] md:aspect-auto object-cover sm:none"
-          />
-          <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
-            Shop now
-          </div>
-        </div>
-
-        <div>
-          <img
-            src="/slider/jeans2.webp"
-            className=" h-[500px] aspect-[16/10] md:aspect-auto object-cover"
+            src="/jeans.webp"
+            className="aspect-[16/10] md:aspect-auto object-cover"
           />
           <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
             Shop now
@@ -49,8 +23,17 @@ export default function HeroBanner() {
         </div>
         <div>
           <img
-            src="/slider/jeans.png"
-            className=" h-[500px] aspect-[16/10] md:aspect-auto object-cover"
+            src="/jeans.webp"
+            className="aspect-[16/10] md:aspect-auto object-cover"
+          />
+          <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
+            Shop now
+          </div>
+        </div>
+        <div>
+          <img
+            src="/jeans.webp"
+            className="aspect-[16/10] md:aspect-auto object-cover"
           />
           <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
             Shop now
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,6 @@
 import HeroBanner from "@/components/HeroBanner"
-import ProductCard from "@/components/ProductCard"
-import Wrapper from "@/components/Wrapper"
 export default function Home() {
-  return <main>
+  return <main className="h-[500px]">
     <HeroBanner />
-    <Wrapper>
-      {/* Heading and paragaph start */}
-      <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
-        <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-          Soy un titulo, largoooo
-        </div>
-        <div className="text-md md:text-xl">
-          lorem ipsum dolor sit amet, consectetur
-          lorem ipsum dolor sit amet, consectetur
-          lorem ipsum dolor sit amet, consectetur
-        </div>
-      </div>
-      {/* Heading and paragaph end */}
-      {/* Products grid start */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-
-      </div>
-      {/* Products grid start */}
-    </Wrapper>
   </main>
 }
